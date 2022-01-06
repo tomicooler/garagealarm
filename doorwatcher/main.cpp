74e9777de34ed6c1e748dda6fae2b39c1a3a36b4
Tamas Domok <domoktamas@gmail.com>
2022-01-06 16:53:08 +0100

    doorwatcher: set TX power to the max.

--- doorwatcher/main.cpp
+++ doorwatcher/main.cpp
@@ -72,6 +72,7 @@ void DoorWatcher::init_encryption() {
 
   LoRa.setSpreadingFactor(12);
   LoRa.setSignalBandwidth(125E3);
+  LoRa.setTxPower(20);
   LoRa.enableCrc();
 
   // IV 16 byte [ xx xx xx xx xx xx xx xx cc cc cc cc cc cc cc cc ]
